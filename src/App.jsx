26f5169f8af02e0fe96b1d8251242da7e540dffd
Rwan Adel <rwan32310@gmail.com>
2024-09-10 18:12:40 +0300

    add landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 //import Navbar from './components/Navbar'
-import Login from './pages/Login'
-import Registration from './pages/Registration'
-import Home from './pages/Home'
-import Logout from './components/Logout'
-import ProtectedRoute from './components/ProtectedRoute'
-import NewPost from './pages/NewPost'
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import Home from "./pages/Home";
+import Logout from "./components/Logout";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NewPost from "./pages/NewPost";
+import Landing from "./pages/Landing";
 
 // <Navbar />
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/sign-up' element={<Registration />}></Route>
+          <Route path="/" element={<Landing />}></Route>
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/sign-up" element={<Registration />}></Route>
           <Route path="/logout" element={<Logout />} />
           <Route path="/NewPost" element={<NewPost />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Landing() {
+  return (
+    <div>
+      <h1>hi</h1>
+      <Link to="/home">to Home</Link>
+    </div>
+  );
+}
